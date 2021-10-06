0fddb7a067fdedb242b903a6b2e9182d56c54a39
tanutcha-coder <72708598+tanutcha-coder@users.noreply.github.com>
2021-10-06 16:37:59 +0700

    first

---
+++ controllers/pages_controller.php
@@ -0,0 +1,13 @@
+<?php
+    class PagesController
+    {
+        public function home()
+        {
+            require_once('views/Pages/home.php');
+        }
+        public function error()
+        {
+            require_once('views/Pages/error.php');
+        }
+    }
+    
